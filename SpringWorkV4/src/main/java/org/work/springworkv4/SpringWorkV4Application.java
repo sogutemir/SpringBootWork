e182b829fc0bd88fabb3ad8d746b4f6371cbba01
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-01-30 14:47:35 +0300

    Created Base Project

---
+++ SpringWorkV4/src/main/java/org/work/springworkv4/SpringWorkV4Application.java
@@ -0,0 +1,13 @@
+package org.work.springworkv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorkV4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWorkV4Application.class, args);
+    }
+
+}
---
+++ SpringWorkV4/src/test/java/org/work/springworkv4/SpringWorkV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.work.springworkv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorkV4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
