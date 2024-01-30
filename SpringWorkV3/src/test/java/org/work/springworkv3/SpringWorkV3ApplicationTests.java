8824365bbb282c81593b0a75599f05fa339e8b94
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-01-30 13:12:26 +0300

    created Project base

---
+++ SpringWorkV3/src/test/java/org/work/springworkv3/SpringWorkV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.work.springworkv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorkV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
